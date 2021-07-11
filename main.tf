385273761583d014876b4ae14233de9037dd0cf4
Кишинский Олег Викторович <OVKishinskiy@sberbank.ru>
2021-07-11 18:24:49 +0300

    optimaize terraform

--- main.tf
+++ main.tf
@@ -38,25 +38,14 @@ resource "aws_security_group" "my_webserver" {
     name = "web-server"
     description = "my security group"
 
-    ingress {
-        from_port = 80
-        to_port = 80
-        protocol = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
-
-    ingress {
-        from_port = 443
-        to_port = 443
-        protocol = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
-
-    ingress {
-        from_port = 22
-        to_port = 22
-        protocol = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
+    dynamic "ingress" {
+            for_each = ["80", "443", "22"]
+            content {
+            from_port = ingress.value
+            to_port = ingress.value
+            protocol = "tcp"
+            cidr_blocks = ["0.0.0.0/0"]
+        }
     }
     egress {
         from_port = 0
